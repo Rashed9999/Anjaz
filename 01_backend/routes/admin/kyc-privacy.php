bcdaf8cd2380309b5a90e208c9784ff761219608
Rashed9999 <105941426+Rashed9999@users.noreply.github.com>
2026-09-14 18:42:51 +0300

    feat(kyc): expose privacy-case operations endpoints

--- 01_backend/routes/admin/kyc-privacy.php
+++ 01_backend/routes/admin/kyc-privacy.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\KycForensicController;
+use App\Http\Controllers\Admin\KycPrivacyAdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('kyc/privacy')
@@ -9,4 +10,7 @@ Route::prefix('kyc/privacy')
     ->group(function () {
         Route::get('/', [KycForensicController::class, 'page'])->name('page');
         Route::get('/trace', [KycForensicController::class, 'lookup'])->name('trace');
+        Route::get('/cases', [KycPrivacyAdminController::class, 'cases'])->name('cases');
+        Route::get('/cases/{userId}', [KycPrivacyAdminController::class, 'show'])
+            ->where('userId', '[0-9]+')->name('cases.show');
     });
