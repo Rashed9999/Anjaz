d068420af53ff633f5fd5c6e954430a3795630b3
Rashed9999 <105941426+Rashed9999@users.noreply.github.com>
2026-09-19 05:50:27 +0300

    fix(rbac): authorize limit changes by target domain

--- 01_backend/routes/admin/user-limits.php
+++ 01_backend/routes/admin/user-limits.php
@@ -21,7 +21,7 @@ Route::middleware(['web', 'admin', 'amial.force-pin-change'])
 
         Route::post('/users/{userId}', [UserLimitCenterController::class, 'update'])
             ->where('userId', '[0-9]+')
-            ->middleware(['platform:platform.customers.limits.update', 'amial.idempotency'])
+            ->middleware('amial.idempotency')
             ->name('users.update');
 
         Route::post('/tiers/{tier}', [UserLimitCenterController::class, 'updateTierPolicy'])
